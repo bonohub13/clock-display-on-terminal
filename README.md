0f3c57b15635fc9cd8e31b41bcf35cc9bc81882f
Kensuke Saito <ken.saito.0813@gmail.com>
2020-05-27 20:20:23 +0900

    Initialized repository

---
+++ README.md
@@ -0,0 +1,5 @@
+# Clock Display on Terminal
+
+## ::::: Updates :::::
+- May 5th, 2020
+	- Initialized repository
---
+++ cpp/src/display_time.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <thread>
+#include <chrono>
+
+class DisplayTime
+{
+public:
+	DisplayTime();
+public:
+	void run();
+	std::string current_time_getter();
+private:
+	std::time_t current_time;
+};
+DisplayTime::DisplayTime()
+	: current_time(std::time(nullptr))
+{}
+void DisplayTime::run()
+{
+	while (true)
+	{
+		std::system("clear");
+		current_time = std::time(nullptr);
+		std::cout << std::asctime(std::localtime(&current_time)) << std::endl;
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+	}
+}
+std::string DisplayTime::current_time_getter()
+{
+	current_time = std::time(nullptr);
+
+	return (std::string)(std::asctime(std::localtime(&current_time)));
+}
+
+int main()
+{
+	DisplayTime time;
+	time.run();
+}
